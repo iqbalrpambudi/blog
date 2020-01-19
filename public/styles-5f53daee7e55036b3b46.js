c0ba2a94089a67e2fe6948b0680a8f0457347c98
iqbalrpambudi <iqbalrpambudi@gmail.com>
2020-01-20 05:56:13 +0700

    add public directory

---
+++ public/styles-5f53daee7e55036b3b46.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"8eCV":function(n,o,i){},GevN:function(n,o,i){},"Mt/m":function(n,o,i){},OElG:function(n,o,i){},QOKM:function(n,o,i){},qHiR:function(n,o,i){},"tb+u":function(n,o,i){}}]);
+//# sourceMappingURL=styles-5f53daee7e55036b3b46.js.map
